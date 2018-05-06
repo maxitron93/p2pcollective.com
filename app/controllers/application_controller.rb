3348c3201f2cb2e4cf61f33eb90bba83784cf034
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-06 21:31:56 +1000

    Fix small typo in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
-profile_path(id: current_user.id)
-if current_user != nil %>
-link_to
-link_to Logiin
-else
