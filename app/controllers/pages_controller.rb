3f5e066021d5e3de4b318a0748153afb6682fb02
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-07 16:45:27 +1000

    Fixed small type in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     all_funded_loans = ActiveLoan.where(status: "funded").or(ActiveLoan.where(status: "settled"))
     @total_amount = 0
     all_funded_loans.each do |loan|
-      loan.opening_balance += total_amount
+      loan.opening_balance += @total_amount
     end
     render layout: "pages"
   end
