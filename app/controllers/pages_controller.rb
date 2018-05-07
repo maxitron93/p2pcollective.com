d9f38dd876d36c07f4125d84bb8a8759ce1a9eee
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-07 16:27:03 +1000

    Styled guest splash page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @funded_active_loans = ActiveLoan.where(status: "unfunded")
+    @funded_active_loans = ActiveLoan.where(status: "unfunded").limit(9)
+    @all_funded_active_loans_count = ActiveLoan.where(status: "unfunded").count
+    all_funded_loans = ActiveLoan.where(status: "funded").or(ActiveLoan.where(status: "settled"))
+    @total_amount = 0
+    all_funded_loans.each do |loan|
+      loan.opening_balance += total_amount
+    end
     render layout: "pages"
   end
 
