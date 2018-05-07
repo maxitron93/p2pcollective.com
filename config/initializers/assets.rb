665d53a01852ade4c3f002f884367c0ed50bcd10
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-07 19:29:02 +1000

    Add styling for signup and login pages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( pages.scss )
+
+Rails.application.config.assets.precompile += %w( devise.css )
