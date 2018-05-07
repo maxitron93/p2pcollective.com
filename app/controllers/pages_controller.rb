af03ce0f3ba3d6c76e3ee132eab052a0155508b1
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-07 15:15:04 +1000

    Started styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,31 @@
 class PagesController < ApplicationController
   def home
+    @funded_active_loans = ActiveLoan.where(status: "unfunded")
+    render layout: "pages"
+  end
+
+  def how_this_works
+
+    render layout: "pages"
+  end
+  
+  def borrow_money
+
+    render layout: "pages"
+  end
+
+  def become_an_investor
+
+    render layout: "pages"
+  end
+
+  def about_us
+
+    render layout: "pages"
+  end
+
+  def contact_us
+
     render layout: "pages"
   end
 end
