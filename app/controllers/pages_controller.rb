4f68d794ea8da97835c9f3d992f169b5c5e9efbe
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-05-10 15:38:25 +1000

    Fix small calculation error in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     all_funded_loans = ActiveLoan.where(status: "funded").or(ActiveLoan.where(status: "settled"))
     @total_amount = 0
     all_funded_loans.each do |loan|
-      @total_amount =+ loan.opening_balance
+      @total_amount += (loan.opening_balance / 100)
     end
     render layout: "pages"
   end
